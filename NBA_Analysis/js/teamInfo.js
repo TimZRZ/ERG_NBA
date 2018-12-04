c2cc582d0f8b60079e632d99f59da04b1626521b
Mine66 <huangzhiwei.ks6@gmail.com>
2018-12-04 17:09:21 +0800

    添加球队基本信息的同步

--- NBA_Analysis/js/teamInfo.js
+++ NBA_Analysis/js/teamInfo.js
@@ -1,45 +1,39 @@
 var url = decodeURI(window.location.href);
-var argsIndex = url .split("?name=");
+var argsIndex = url.split("?name=");
 var arg = argsIndex[1];
 var name = arg.split("%20")[0];
+var name = name.split(",")[0];
 var query_name = new AV.Query('Team');
-query_name.equalTo('Name',name);
+query_name.equalTo('Franchise',name);
 query_name.find().then(function(todo){
-  var team_name = todo[0].get('Name');
-  var team_height = todo[0].get('Ht');
-  var team_weight = todo[0].get('Wt');
-  var team_position = todo[0].get('Pos');
-//   还有四个参数未确定
-//   var player_birth = todo[0].get('Birth_Date');
-//   var player_begin = todo[0].get('From');
-//   var player_university = todo[0].get('Colleges');
+  var team_name = todo[0].get('Franchise');
+  var team_conference = todo[0].get('Conference');
+  var team_abbr = todo[0].get('Abbreviation');
+  var team_from = todo[0].get('From');
+  var team_to = todo[0].get('To');
+  var team_game = todo[0].get('G');
+  var team_win_rate = todo[0].get('W_L');
+  var team_playoff = todo[0].get('Plyfs');
+  var team_champion = todo[0].get('Champ');
+  //同步球队照片
+  var query_photo = new AV.Query('Team_Image');
+  query_photo.equalTo('Tm', team_abbr);
+  query_photo.find().then(function(todo){
+    var team_logo = todo[0].get('url');
+    document.getElementById('team_logo').src = team_logo;
+  });
+
   
-  //同步球队各项信息
-  document.getElementById("name").innerHTML= player_name;
-  document.getElementById("player_height").innerHTML=player_height;
-  document.getElementById("player_weight").innerHTML=player_weight;
-  document.getElementById("player_position").innerHTML=player_position;
-  document.getElementById("player_birth").innerHTML=player_birth;
-  document.getElementById("player_begin").innerHTML=player_begin;
-  document.getElementById("player_university").innerHTML=player_university;
-});
-//同步球队照片
-var query_photo = new AV.Query('Player_Image');
-query_photo.equalTo('Name',name.toLowerCase());
-query_photo.find().then(function(todo){
-  var player_photo = todo[0].get('Photo');
-  document.getElementById('playerPhoto').src = player_photo;
-});
-//同步球员号码和球队信息
-var query_teamInfo = new AV.Query('Player_Team');
-query_teamInfo.equalTo('Player',name);
+  //同步球队前四项
+  document.getElementById("team_name").innerHTML= team_name;
+  document.getElementById("team_conference").innerHTML=team_conference;
+  document.getElementById("team_abbr").innerHTML=team_abbr;
+  document.getElementById("team_from").innerHTML=team_from;
+  document.getElementById("team_to").innerHTML=team_to;
+  document.getElementById("team_game").innerHTML=team_game;
+  document.getElementById("team_win_rate").innerHTML=team_win_rate;
+  document.getElementById("team_playoff").innerHTML=team_playoff;
+  document.getElementById("team_champion").innerHTML=team_champion;
 
-query_teamInfo.find().then(function(todo){
-  var player_number = todo[0].get('No_');
-  var player_team = todo[0].get('Team');
+  });
 
-  document.getElementById('player_number').innerHTML = player_number;
-  document.getElementById('player_team').innerHTML = player_team;
-}), function(error){
-
-};
